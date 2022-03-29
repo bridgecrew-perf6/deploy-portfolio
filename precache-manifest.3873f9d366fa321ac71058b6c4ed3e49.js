1371aa4e0856537f9387fdb19842bdd1c3880e8b
Rachel Thiim <rachel.thiim@gmail.com>
2022-03-29 10:58:33 -0500

    Updates

---
+++ precache-manifest.3873f9d366fa321ac71058b6c4ed3e49.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc456a55a0127a039ecc63b2c1d5bd43",
+    "url": "/deploy-portfolio/index.html"
+  },
+  {
+    "revision": "af69305313ce7dda5746",
+    "url": "/deploy-portfolio/static/css/main.86e6e9f4.chunk.css"
+  },
+  {
+    "revision": "ec7dea27f2d267470dff",
+    "url": "/deploy-portfolio/static/js/2.102548e6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/deploy-portfolio/static/js/2.102548e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af69305313ce7dda5746",
+    "url": "/deploy-portfolio/static/js/main.0290343a.chunk.js"
+  },
+  {
+    "revision": "098bce34b8d434f0929e",
+    "url": "/deploy-portfolio/static/js/runtime-main.9b3cb7e6.js"
+  },
+  {
+    "revision": "a19f2b67a0540f784bf091db8e36c82a",
+    "url": "/deploy-portfolio/static/media/calculator.a19f2b67.jpg"
+  },
+  {
+    "revision": "402a471b5aad190a33b649f1368f5de1",
+    "url": "/deploy-portfolio/static/media/cover-image.402a471b.jpg"
+  },
+  {
+    "revision": "653c2b3d29f66a3c6e957518f3bd16f0",
+    "url": "/deploy-portfolio/static/media/led-wall.653c2b3d.jpg"
+  },
+  {
+    "revision": "ed494730868bd902e0ee98f1df1877e2",
+    "url": "/deploy-portfolio/static/media/pastel-puzzels.ed494730.jpg"
+  },
+  {
+    "revision": "938f8fe0fe2e5880b3ef528c5188a096",
+    "url": "/deploy-portfolio/static/media/run-buddy.938f8fe0.jpg"
+  },
+  {
+    "revision": "12557c9b108cf5e5347f8d838014b31e",
+    "url": "/deploy-portfolio/static/media/surf-report.12557c9b.jpg"
+  }
+]);
